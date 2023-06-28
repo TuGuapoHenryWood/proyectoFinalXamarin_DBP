890053b8105335037dd7bd438ae19f87faccdd90
TuGuapoHenryWood <101741266+TuGuapoHenryWood@users.noreply.github.com>
2023-06-27 21:37:40 -0500

    Update Page1.xaml.cs

--- Page1.xaml.cs
+++ Page1.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.SimpleAudioPlayer;
+[assembly: ExportFont("Lexend-Regular.ttf", Alias = "Lexend")]
 [assembly: ExportFont("Caprasimo-Regular.ttf", Alias = "Caprasimo")]
 
 namespace proyectoFinal_DBP
@@ -21,11 +25,21 @@ namespace proyectoFinal_DBP
         public void MovePage(object obj, EventArgs args)
         {
             Navigation.PushAsync(new Study());
+            Player("Sonidos.The Beatles A Hard Day's Night Full Album.mp3");
         }
 
         public void MoveTo(object obj, EventArgs args)
         {
             Navigation.PushAsync(new Nuevo());
+        }
+ 
+        public void Player(string sonido)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream audioStream = assembly.GetManifestResourceStream(sonido);
+            var audio = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            audio.Load(audioStream);
+            audio.Play();
         }
     }
 }
